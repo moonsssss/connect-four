c8d18f3419f7c644e24ade4a37b1575fe4db7241
Lochlann O’Connell <lochlann@Lochlanns-MBP.localdomain>
2023-11-12 16:02:36 -0800

    Worked on testing code

--- Testing.py
+++ Testing.py
@@ -6,6 +6,7 @@ import pytest
 ##############################
 # To install pytest, run pip install pytest
 # To run the tests, type in: python -m pytest Testing.py
+# To run the tests on MAC, type in: pytest Testing.py
 
 ###############################
 NUM_COLUMNS = 7
@@ -16,14 +17,47 @@ def drop_piece(board, row, column, piece):
     """
     This will set the specified spot in the board to the piece (1 for player 1, 2 for player 2)
     """
-    print("TODO!")
-
+    board[row][column] = piece
 def is_valid_location(board, column):
     """
     This function checks to make sure the column (that the player wants
     to drop the piece in) is not full
     """
-    print("TODO!")
+    if board[5][column] == 0:
+        return True
+    return False
+ 
+def get_next_open_row(board, column):
+    """
+    Go through the rows (up and down) and find the first opening that's free.
+    If you find an opening, return that row number
+    """
+    # 1) Loop through the rows
+        # 2) If the column the player wants to drop their piece in is open,
+        # return that row
+    pass
+
+###############################
+
+def is_this_winning_move(board, piece):
+    """
+    After a player puts down their piece (1 or 2), we want to check if they just made
+    a winning move.
+    1) Check the horizontal locations, return True if you find a winning movez
+    2) Check the vertical locations, return True if you find a winning movez
+    3) Check diaganols going up and to the left, return True if you find a winning movez
+    4) Check diaganols going up and to the right, return True if you find a winning movez
+    Return False if we don't find a winning move.
+    """
+    # Check the horizontal locations
+
+    # Check the vertical locations
+
+    # Check diaganols going up and to the left
+
+    # Check diaganols going up and to the right
+
+    return False
 ###############################
 # Tests (no need to change these!)
 
@@ -159,3 +193,313 @@ def test_drop_piece_2():
     print_board(board)
     drop_piece(board=board, row=0, column=0, piece=2)
     assert board[0][0] == 2
+
+
+def test_get_next_open_row_1():
+    """
+    Testing get_next_open_row(board, column).
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 0, 2, 1, 0, 1], #R0
+        [1, 1, 0, 0, 2, 0, 2], #R1
+        [2, 2, 0, 0, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 2], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 2]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 0, 1, 0, 2]
+    # R1: [1, 1, 0, 0, 2, 0, 2]
+    # R0: [1, 1, 0, 2, 1, 0, 1]
+    print_board(board)
+    row = get_next_open_row(board=board,column=0)
+    assert row == 4
+
+def test_get_next_open_row_2():
+    """
+    Testing get_next_open_row(board, column).
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 0, 2, 1, 0, 1], #R0
+        [1, 1, 0, 0, 2, 0, 2], #R1
+        [2, 2, 0, 0, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 2], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 2]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 0, 1, 0, 2]
+    # R1: [1, 1, 0, 0, 2, 0, 2]
+    # R0: [1, 1, 0, 2, 1, 0, 1]
+    print_board(board)
+    row = get_next_open_row(board=board,column=2)
+    assert row == 0
+
+def test_get_next_open_row_3():
+    """
+    Testing get_next_open_row(board, column).
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 0, 2, 1, 0, 1], #R0
+        [1, 1, 0, 0, 2, 0, 2], #R1
+        [2, 2, 0, 0, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 0]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 0, 1, 0, 2]
+    # R1: [1, 1, 0, 0, 2, 0, 2]
+    # R0: [1, 1, 0, 2, 1, 0, 1]
+    print_board(board)
+    row = get_next_open_row(board=board,column=6)
+    assert row == 5
+
+def test_not_winning_move_1():
+    """
+    Testing is_this_winning_move(board, piece).
+    No winning move
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 0, 2, 1, 0, 1], #R0
+        [1, 1, 0, 0, 2, 0, 2], #R1
+        [2, 2, 0, 0, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 0]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 0, 1, 0, 2]
+    # R1: [1, 1, 0, 0, 2, 0, 2]
+    # R0: [1, 1, 0, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=1)
+    assert is_winning_move == False
+
+def test_not_winning_move_2():
+    """
+    Testing is_this_winning_move(board, piece).
+    Not winning move
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 0, 2, 1, 0, 1], #R0
+        [1, 1, 0, 0, 2, 0, 2], #R1
+        [2, 2, 0, 0, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 0]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 0, 1, 0, 2]
+    # R1: [1, 1, 0, 0, 2, 0, 2]
+    # R0: [1, 1, 0, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=2)
+    assert is_winning_move == False
+
+def test_not_winning_move_3():
+    """
+    Testing is_this_winning_move(board, piece).
+    Not winning move
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 1, 2, 1, 0, 1], #R0
+        [1, 1, 1, 1, 2, 0, 2], #R1
+        [2, 2, 0, 0, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 0]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 0, 1, 0, 2]
+    # R1: [1, 1, 1, 1, 2, 0, 2]
+    # R0: [1, 1, 1, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=2)
+    assert is_winning_move == False
+
+def test_horizontal_winning_move_1():
+    """
+    Testing is_this_winning_move(board, piece).
+    Horizontal winning move
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 1, 2, 1, 0, 1], #R0
+        [1, 1, 1, 1, 2, 0, 2], #R1
+        [2, 2, 0, 0, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 0]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 0, 1, 0, 2]
+    # R1: [1, 1, 1, 1, 2, 0, 2]
+    # R0: [1, 1, 1, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=1)
+    assert is_winning_move == True
+
+def test_horizontal_winning_move_2():
+    """
+    Testing is_this_winning_move(board, piece).
+    Horizontal winning move for player 2
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 1, 2, 1, 0, 1], #R0
+        [1, 1, 2, 1, 2, 0, 2], #R1
+        [2, 2, 2, 2, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 0]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 2, 2, 1, 0, 2]
+    # R1: [1, 1, 2, 1, 2, 0, 2]
+    # R0: [1, 1, 1, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=2)
+    assert is_winning_move == True
+
+def test_horizontal_winning_move_3():
+    """
+    Testing is_this_winning_move(board, piece).
+    Not horizontal winning move for player 1
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 1, 2, 1, 0, 1], #R0
+        [1, 1, 2, 1, 2, 0, 2], #R1
+        [2, 2, 2, 2, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 0]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 2, 2, 1, 0, 2]
+    # R1: [1, 1, 2, 1, 2, 0, 2]
+    # R0: [1, 1, 1, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=1)
+    assert is_winning_move == False
+
+def test_vertical_winning_move_1():
+    """
+    Testing is_this_winning_move(board, piece).
+    Not vertical winning move
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 1, 2, 1, 0, 1], #R0
+        [1, 1, 2, 1, 2, 0, 2], #R1
+        [2, 2, 2, 2, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [0, 1, 0, 0, 0, 0, 1], #R4
+        [0, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [0, 1, 0, 0, 0, 0, 0]
+    # R4: [0, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 2, 2, 1, 0, 2]
+    # R1: [1, 1, 2, 1, 2, 0, 2]
+    # R0: [1, 1, 1, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=1)
+    assert is_winning_move == False
+
+def test_vertical_winning_move_2():
+    """
+    Testing is_this_winning_move(board, piece).
+    Vertical winning move for player 2
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 1, 2, 1, 0, 1], #R0
+        [1, 1, 2, 1, 2, 0, 2], #R1
+        [2, 2, 0, 2, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [2, 1, 0, 0, 0, 0, 1], #R4
+        [2, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [2, 1, 0, 0, 0, 0, 0]
+    # R4: [2, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 2, 1, 0, 2]
+    # R1: [1, 1, 2, 1, 2, 0, 2]
+    # R0: [1, 1, 1, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=2)
+    assert is_winning_move == True
+
+def test_vertical_winning_move_3():
+    """
+    Testing is_this_winning_move(board, piece).
+    Not Vertical winning move for player 1
+    """
+    board = [
+       # C0,C1,C2,C3,C4,C5,C6
+        [1, 1, 1, 2, 1, 0, 1], #R0
+        [1, 1, 2, 1, 2, 0, 2], #R1
+        [2, 2, 0, 2, 1, 0, 2], #R2
+        [2, 1, 0, 0, 0, 0, 2], #R3
+        [2, 1, 0, 0, 0, 0, 1], #R4
+        [2, 1, 0, 0, 0, 0, 0], #R5
+    ]
+    # This board looks like this to us:
+    #      C0 C1 C2 C3 C4 C5 C6
+    # R5: [2, 1, 0, 0, 0, 0, 0]
+    # R4: [2, 1, 0, 0, 0, 0, 1]
+    # R3: [2, 1, 0, 0, 0, 0, 2]
+    # R2: [2, 2, 0, 2, 1, 0, 2]
+    # R1: [1, 1, 2, 1, 2, 0, 2]
+    # R0: [1, 1, 1, 2, 1, 0, 1]
+    print_board(board)
+    is_winning_move = is_this_winning_move(board=board,piece=1)
+    assert is_winning_move == False
